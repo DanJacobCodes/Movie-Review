4d6d6411137ff8a6f135c1e2d1392dd6373ebaa8
Dan Jacob and David Shin <dan+david@>
2017-05-31 16:39:10 -0700

    add more styling

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -9,12 +9,12 @@ export default Ember.Component.extend({
 
     saveReview1() {
       var params = {
-        user: this.get('user'),
-        movie: this.get('movie'),
-        director: this.get('director'),
-        genre: this.get('genre'),
-        user_review: this.get('user_review'),
-        image: this.get('image'),
+        user: this.get('user') ? this.get('user'): "",
+        movie: this.get('movie') ? this.get('movie'): "",
+        director: this.get('director') ? this.get('director'): "" ,
+        genre: this.get('genre') ? this.get('genre'): "",
+        user_review: this.get('user_review') ? this.get('user_review'): "",
+        image: this.get('image') ? this.get('image'): "",
       };
       this.set('addNewReview', false);
       this.sendAction('saveReview2', params);
